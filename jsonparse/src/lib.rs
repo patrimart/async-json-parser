247972ce264ffa667f57ec2bb31d38bf74bf13a2
Patrick Martin <bypatrickmartin@gmail.com>
2022-01-03 23:13:54 -0800

    add external interface

--- jsonparse/src/lib.rs
+++ jsonparse/src/lib.rs
@@ -2,18 +2,17 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub fn create_parser(path_to_array: Option<Box<[JsValue]>>) -> String {
+    todo!("");
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, jsonparse!");
+pub fn write(handle_id: String, uint8_array: Box<[u8]>) {
+    todo!("");
+}
+
+#[wasm_bindgen]
+pub fn poll(handle_id: String) -> Option<Box<[u8]>> {
+    todo!("");
 }
