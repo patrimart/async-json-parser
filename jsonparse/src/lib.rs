11853ba2735ca4eeda30784ef9599e3db81ab7c0
Patrick Martin <bypatrickmartin@gmail.com>
2022-01-03 23:55:28 -0800

    Finalize and comment on external interface

--- jsonparse/src/lib.rs
+++ jsonparse/src/lib.rs
@@ -1,6 +1,16 @@
 mod utils;
 
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+
 use wasm_bindgen::prelude::*;
+
+lazy_static! {
+    // <ref_id, JsonParser>
+    static ref PARSER_MAP: HashMap<String, String> = HashMap::new();
+}
 
 #[wasm_bindgen]
 pub enum PollResponse {
@@ -9,21 +19,30 @@ pub enum PollResponse {
 }
 
 #[wasm_bindgen]
-pub fn create_parser(path_to_array: Option<Box<[JsValue]>>) -> String {
-    todo!("");
-}
-
-#[wasm_bindgen]
-pub fn write(handle_id: String, uint8_array: Box<[u8]>) {
-    todo!("");
+/// Intializes the JsonParser and returns a ref_id for the new parser.
+/// An optional argumant provides the path to the array to stream. Omit if array.
+pub fn create_json_parser(path_to_array: Option<Box<[JsValue]>>) -> String {
+    todo!("Not implemented");
 }
 
 #[wasm_bindgen(catch)]
-pub fn poll(handle_id: String) -> Result<Box<[u8]>, JsValue> {
-    todo!("");
+/// Appends additional streaming data to the JsonParser.
+/// Throws PollResponse.
+pub fn push(ref_id: String, uint8_array: Box<[u8]>) -> Result<(), JsValue> {
+    todo!("Not implemented");
+}
+
+#[wasm_bindgen(catch)]
+/// Attempts to collect and return the next item in the streaming data.
+/// Throws if ref_id does not exist.
+pub fn poll(ref_id: String) -> Result<Box<[u8]>, JsValue> {
+    todo!("Not implemented");
 }
 
 #[wasm_bindgen]
-pub fn cleanup(handle_id: String) {
-    todo!("");
+/// Call when the JsonParser is no longer needed.
+/// Fails silently if ref_id does not exist.
+pub fn destroy(ref_id: String) {
+    
+    todo!("Not implemented");
 }
