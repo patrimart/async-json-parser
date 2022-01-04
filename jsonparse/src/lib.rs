76f354ccb3af9e4acfd574807086e117324bcfbc
Patrick Martin <bypatrickmartin@gmail.com>
2022-01-03 23:31:57 -0800

    finalize external interface

--- jsonparse/src/lib.rs
+++ jsonparse/src/lib.rs
@@ -3,6 +3,12 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub enum PollResponse {
+    Pending = 0,
+    Done = 1,
+}
+
+#[wasm_bindgen]
 pub fn create_parser(path_to_array: Option<Box<[JsValue]>>) -> String {
     todo!("");
 }
@@ -12,7 +18,12 @@ pub fn write(handle_id: String, uint8_array: Box<[u8]>) {
     todo!("");
 }
 
+#[wasm_bindgen(catch)]
+pub fn poll(handle_id: String) -> Result<Box<[u8]>, JsValue> {
+    todo!("");
+}
+
 #[wasm_bindgen]
-pub fn poll(handle_id: String) -> Option<Box<[u8]>> {
+pub fn cleanup(handle_id: String) {
     todo!("");
 }
