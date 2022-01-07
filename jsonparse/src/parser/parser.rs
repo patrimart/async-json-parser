f3c46f8c0f7a5c00744b3c4783d8f3d5d585e863
Patrick Martin <bypatrickmartin@gmail.com>
2022-01-06 23:46:15 -0800

    init Parser

--- jsonparse/src/parser/parser.rs
+++ jsonparse/src/parser/parser.rs
@@ -1,12 +1,15 @@
+use lazy_static::__Deref;
+
 use super::constants::*;
 
 pub enum PollResponse {
-    Next(&[u8]),
+    Next(Box<[u8]>),
     Pending,
     Done,
 }
 
 pub struct Parser {
+    path_to_array: Box<[String]>,
     state: ParseState,
     current_path: Vec<String>,
     depth_stack: Vec<u8>,
@@ -15,17 +18,24 @@ pub struct Parser {
 }
 
 impl Parser {
-    pub fn new() -> Self {
+    pub fn new(path_to_array: &[String]) -> Self {
         Parser {
+            path_to_array: Box::new(path_to_array.clone()),
             state: ParseState::AtStart,
             current_path: Vec::new(),
             depth_stack: Vec::new(),
             current_index: 0,
-            stream: Vec::new(),,
+            stream: Vec::new(),
         }
     }
 
-    pub fn push(&mut self, arr: &[u8]) {}
+    pub fn push(&mut self, arr: &[u8]) {
+        for item in arr.iter() {
+            self.stream.push(*item);
+        }
+    }
 
-    pub fn poll(&self) -> PollResponse {}
+    pub fn poll(&self) -> PollResponse {
+        PollResponse::Pending
+    }
 }
