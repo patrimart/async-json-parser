0841759376282320ab3990d2d880843211414e2c
Patrick Martin <bypatrickmartin@gmail.com>
2022-01-08 21:45:59 -0800

    verify constants test

--- jsonparse/src/parser/constants.rs
+++ jsonparse/src/parser/constants.rs
@@ -30,15 +30,70 @@ mod tests {
     #[test]
     fn verify_constants() {
         let json = b"{ \"foo\": [1, 2, 3], \"bar\": true }";
-        let mut open_bracket = 0;
+        let mut back_slash_count = 0;
+        let mut bracket_open_count = 0;
+        let mut bracket_close_count = 0;
+        let mut colon_count = 0;
+        let mut comma_count = 0;
+        let mut curly_bracket_open_count = 0;
+        let mut curly_bracket_close_count = 0;
+        let mut double_quote_count = 0;
+        let mut white_space_count = 0;
+        let mut utf8_2_count = 0;
+        let mut utf8_3_count = 0;
+        let mut utf8_4_count = 0;
         for c in json.iter() {
             match *c {
+                BACK_SLASH => {
+                    back_slash_count = back_slash_count + 1;
+                }
+                BRACKET_OPEN => {
+                    bracket_open_count = bracket_open_count + 1;
+                }
+                BRACKET_CLOSE => {
+                    bracket_close_count = bracket_close_count + 1;
+                }
+                COLON => {
+                    colon_count = colon_count + 1;
+                }
+                COMMA => {
+                    comma_count = comma_count + 1;
+                }
                 CURLY_BRACKET_OPEN => {
-                    open_bracket = open_bracket + 1;
+                    curly_bracket_open_count = curly_bracket_open_count + 1;
+                }
+                CURLY_BRACKET_CLOSE => {
+                    curly_bracket_close_count = curly_bracket_close_count + 1;
+                }
+                DOUBLE_QUOTE => {
+                    double_quote_count = double_quote_count + 1;
+                }
+                WHITE_SPACE => {
+                    white_space_count = white_space_count + 1;
+                }
+                UTF8_2 => {
+                    utf8_2_count = utf8_2_count + 1;
+                }
+                UTF8_3 => {
+                    utf8_3_count = utf8_3_count + 1;
+                }
+                UTF8_4 => {
+                    utf8_4_count = utf8_4_count + 1;
                 }
                 _ => {}
             }
         }
-        assert_eq!(open_bracket, 1);
+        assert_eq!(back_slash_count, 0);
+        assert_eq!(bracket_open_count, 1);
+        assert_eq!(bracket_close_count, 1);
+        assert_eq!(colon_count, 2);
+        assert_eq!(comma_count, 3);
+        assert_eq!(curly_bracket_open_count, 1);
+        assert_eq!(curly_bracket_close_count, 1);
+        assert_eq!(double_quote_count, 4);
+        assert_eq!(white_space_count, 7);
+        assert_eq!(utf8_2_count, 0);
+        assert_eq!(utf8_3_count, 0);
+        assert_eq!(utf8_4_count, 0);
     }
 }
